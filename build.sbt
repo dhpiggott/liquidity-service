22627c794402437fb3155ce517a867d5603ad5ba
David Piggott <david@piggott.me.uk>
2015-05-26 17:17:05 +0100

    Switch from Apache Commons Codec to Google Guava
    Android includes an older version of Apache Commons Codec that lacks the base64
    method(s) we were using. Hence the need to locate the functionality in a
    different namespace.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "liquidity-common"
 
 organization := "com.dhpcs"
 
-version := "0.3.0"
+version := "0.4.0-SNAPSHOT"
 
 scalaVersion := "2.11.6"
 
@@ -11,5 +11,6 @@ resolvers += "Pellucid Bintray" at "https://dl.bintray.com/pellucid/maven"
 libraryDependencies ++= Seq(
   "com.pellucid" %% "sealerate" % "0.0.3",
   "com.typesafe.play" %% "play-json" % "2.3.8",
-  "commons-codec" % "commons-codec" % "1.10"
+  "com.google.guava" % "guava" % "18.0",
+  "com.google.code.findbugs" % "jsr305" % "3.0.0"
 )
--- src/main/scala/com/dhpcs/liquidity/models/PublicKey.scala
+++ src/main/scala/com/dhpcs/liquidity/models/PublicKey.scala
@@ -3,12 +3,12 @@ package com.dhpcs.liquidity.models
 import java.security.MessageDigest
 import java.util
 
-import org.apache.commons.codec.binary.{Base64, Hex}
+import com.google.common.io.BaseEncoding
 import play.api.libs.json._
 
 case class PublicKey(value: Array[Byte]) {
 
-  lazy val fingerprint = Hex.encodeHexString(MessageDigest.getInstance("SHA-1").digest(value))
+  lazy val fingerprint = BaseEncoding.base16().encode(MessageDigest.getInstance("SHA-1").digest(value))
 
   override def equals(that: Any) = that match {
 
@@ -20,17 +20,17 @@ case class PublicKey(value: Array[Byte]) {
 
   override def hashCode = util.Arrays.hashCode(value)
 
-  override def toString = s"$productPrefix(${Base64.encodeBase64String(value)})"
+  override def toString = s"$productPrefix(${BaseEncoding.base64().encode(value)})"
 
 }
 
 object PublicKey {
 
   implicit val publicKeyReads =
-    __.read[String].map(publicKeyBase64 => PublicKey(Base64.decodeBase64(publicKeyBase64)))
+    __.read[String].map(publicKeyBase64 => PublicKey(BaseEncoding.base64().decode(publicKeyBase64)))
 
   implicit val publicKeyWrites = Writes[PublicKey] {
-    publicKey => JsString(Base64.encodeBase64String(publicKey.value))
+    publicKey => JsString(BaseEncoding.base64().encode(publicKey.value))
   }
 
 }

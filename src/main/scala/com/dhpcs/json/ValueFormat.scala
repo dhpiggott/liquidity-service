8352f0ff7c8ba55a610870697847100b386f94c9
David Piggott <david@piggott.me.uk>
2015-06-06 16:54:44 +0100

    Make ValueFormat reads consistent with style used in JsonRpcMessage

--- src/main/scala/com/dhpcs/json/ValueFormat.scala
+++ src/main/scala/com/dhpcs/json/ValueFormat.scala
@@ -1,11 +1,11 @@
 package com.dhpcs.json
 
-import play.api.libs.json.{Format, Reads, Writes}
+import play.api.libs.json.{Format, Writes, _}
 
 object ValueFormat {
 
   def apply[V, W: Format](apply: W => V, unapply: V => W) = Format(
-    Reads.of[W].map(apply),
+    __.read[W].map(apply),
     Writes[V](a => Writes.of[W].writes(unapply(a)))
   )
 

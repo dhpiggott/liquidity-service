4bf3f3ba1e9fc72636db495fdceaf54be98699e1
David Piggott <david@piggott.me.uk>
2015-10-01 14:18:40 +0100

    Use updated nginx-proxy logging config, remove server_tokens off config

--- nginx.sh
+++ nginx.sh
@@ -9,6 +9,7 @@ sudo docker run -d -p 443:443 \
     -v $DIR/nginx/dh2048.pem:/etc/nginx/certs/dh2048.pem:ro \
     -v $DIR/nginx/liquidity.dhpcs.com.crt:/etc/nginx/certs/liquidity.dhpcs.com.crt:ro \
     -v $DIR/nginx/liquidity.dhpcs.com.key:/etc/nginx/certs/liquidity.dhpcs.com.key:ro \
+    -v $DIR/nginx/liquidity.dhpcs.com.dhparam.pem:/etc/nginx/certs/liquidity.dhpcs.com.dhparam.pem:ro \
     -v $DIR/nginx/default.conf:/etc/nginx/conf.d/default.conf:ro \
     --link liquidity:liquidity \
     nginx:1

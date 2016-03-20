50f566e263c430227bc250f06c9a0219a092a0fb
David Piggott <david@piggott.me.uk>
2016-03-20 12:24:05 +0000

    Use TestProbe in ZoneValidatorSpec in order to avoid ImplicitSender
    ImplicitSender is not reset between tests, so can lead to message receipts
    triggered by one test causing false result in a subsequent test that reads
    from the ImplicitSender's mailbox via expect* methods.

--- test/actors/ZoneValidatorSpec.scala
+++ test/actors/ZoneValidatorSpec.scala
@@ -7,7 +7,7 @@ import actors.ClientConnection.MessageReceivedConfirmation
 import actors.ZoneValidator.{AuthenticatedCommandWithIds, CommandReceivedConfirmation, EnvelopedMessage, ResponseWithIds}
 import akka.actor.ActorSystem
 import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
-import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
+import akka.testkit.{DefaultTimeout, TestKit, TestProbe}
 import com.dhpcs.jsonrpc.{ErrorResponse, JsonRpcResponseError}
 import com.dhpcs.liquidity.models._
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
@@ -15,8 +15,8 @@ import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 import scala.concurrent.duration._
 import scala.util.Left
 
-class ZoneValidatorSpec(system: ActorSystem) extends TestKit(system) with DefaultTimeout with ImplicitSender
-  with WordSpecLike with BeforeAndAfterAll {
+class ZoneValidatorSpec(system: ActorSystem) extends TestKit(system)
+  with WordSpecLike with DefaultTimeout with BeforeAndAfterAll {
 
   private val publicKey = {
     val publicKeyBytes = KeyPairGenerator.getInstance("RSA").generateKeyPair.getPublic.getEncoded
@@ -31,6 +31,8 @@ class ZoneValidatorSpec(system: ActorSystem) extends TestKit(system) with Defaul
     extractShardId = ZoneValidator.extractShardId
   )
 
+  private implicit val sys = system
+
   private val unusedClientCorrelationId = Option(Left("unused"))
   private val unusedClientDeliveryId = 0L
 
@@ -38,25 +40,27 @@ class ZoneValidatorSpec(system: ActorSystem) extends TestKit(system) with Defaul
 
   def this() = this(ActorSystem("application"))
 
-  override def afterAll() {
-    shutdown()
-  }
+  override def afterAll() = TestKit.shutdownActorSystem(system)
 
   "A ZoneValidator" must {
     "send an error response when joined before creation" in {
+      val testProbe = TestProbe()
       val zoneId = ZoneId.generate
       val sequenceNumber = commandSequenceNumbers(zoneId)
       commandSequenceNumbers = commandSequenceNumbers + (zoneId -> (sequenceNumber + 1))
-      zoneValidatorShardRegion ! AuthenticatedCommandWithIds(
-        publicKey,
-        JoinZoneCommand(
-          zoneId
-        ),
-        unusedClientCorrelationId,
-        sequenceNumber,
-        unusedClientDeliveryId
+      testProbe.send(
+        zoneValidatorShardRegion,
+        AuthenticatedCommandWithIds(
+          publicKey,
+          JoinZoneCommand(
+            zoneId
+          ),
+          unusedClientCorrelationId,
+          sequenceNumber,
+          unusedClientDeliveryId
+        )
       )
-      expectMsgPF(10.seconds) {
+      testProbe.expectMsgPF(10.seconds) {
         case CommandReceivedConfirmation(`zoneId`, _) =>
       }
       val expectedResponse = Left(
@@ -65,67 +69,83 @@ class ZoneValidatorSpec(system: ActorSystem) extends TestKit(system) with Defaul
           "Zone does not exist"
         )
       )
-      val zoneDeliveryId = expectMsgPF() {
+      val zoneDeliveryId = testProbe.expectMsgPF() {
         case ResponseWithIds(`expectedResponse`, `unusedClientCorrelationId`, _, id) => id
       }
-      lastSender ! MessageReceivedConfirmation(zoneDeliveryId)
+      testProbe.send(
+        testProbe.lastSender,
+        MessageReceivedConfirmation(zoneDeliveryId)
+      )
     }
   }
 
   "A ZoneValidator" must {
     "send a create zone response when a zone is created" in {
+      val testProbe = TestProbe()
       val zoneId = ZoneId.generate
       val sequenceNumber = commandSequenceNumbers(zoneId)
       commandSequenceNumbers = commandSequenceNumbers + (zoneId -> (sequenceNumber + 1))
-      zoneValidatorShardRegion ! EnvelopedMessage(
-        zoneId,
-        AuthenticatedCommandWithIds(
-          publicKey,
-          CreateZoneCommand(
+      testProbe.send(
+        zoneValidatorShardRegion,
+        EnvelopedMessage(
+          zoneId,
+          AuthenticatedCommandWithIds(
             publicKey,
-            Some("Dave"),
-            None,
-            None,
-            None,
-            Some("Dave's Game")
+            CreateZoneCommand(
+              publicKey,
+              Some("Dave"),
+              None,
+              None,
+              None,
+              Some("Dave's Game")
+            ),
+            unusedClientCorrelationId,
+            sequenceNumber,
+            unusedClientDeliveryId
+          )
+        )
+      )
+      testProbe.expectMsgPF(10.seconds) {
+        case CommandReceivedConfirmation(`zoneId`, _) =>
+      }
+      val zoneDeliveryId = testProbe.expectMsgPF() {
+        case ResponseWithIds(Right(CreateZoneResponse(_)), `unusedClientCorrelationId`, _, id) => id
+      }
+      testProbe.send(
+        testProbe.lastSender,
+        MessageReceivedConfirmation(zoneDeliveryId)
+      )
+    }
+  }
+
+  "A ZoneValidator" must {
+    "send a JoinZoneResponse when joined" in {
+      val testProbe = TestProbe()
+      val zoneId = ZoneId(UUID.fromString("4cdcdb95-5647-4d46-a2f9-a68e9294d00a"))
+      val sequenceNumber = commandSequenceNumbers(zoneId)
+      commandSequenceNumbers = commandSequenceNumbers + (zoneId -> (sequenceNumber + 1))
+      testProbe.send(
+        zoneValidatorShardRegion, AuthenticatedCommandWithIds(
+          publicKey,
+          JoinZoneCommand(
+            zoneId
           ),
           unusedClientCorrelationId,
           sequenceNumber,
           unusedClientDeliveryId
         )
       )
-      expectMsgPF(10.seconds) {
-        case CommandReceivedConfirmation(`zoneId`, _) =>
-      }
-      val zoneDeliveryId = expectMsgPF() {
-        case ResponseWithIds(Right(CreateZoneResponse(_)), `unusedClientCorrelationId`, _, id) => id
-      }
-      lastSender ! MessageReceivedConfirmation(zoneDeliveryId)
-    }
-  }
-
-  "A ZoneValidator" must {
-    "send a JoinZoneResponse when joined" in {
-      val zoneId = ZoneId(UUID.fromString("4cdcdb95-5647-4d46-a2f9-a68e9294d00a"))
-      val sequenceNumber = commandSequenceNumbers(zoneId)
-      commandSequenceNumbers = commandSequenceNumbers + (zoneId -> (sequenceNumber + 1))
-      zoneValidatorShardRegion ! AuthenticatedCommandWithIds(
-        publicKey,
-        JoinZoneCommand(
-          zoneId
-        ),
-        unusedClientCorrelationId,
-        sequenceNumber,
-        unusedClientDeliveryId
-      )
-      expectMsgPF(10.seconds) {
+      testProbe.expectMsgPF(10.seconds) {
         case CommandReceivedConfirmation(`zoneId`, _) =>
       }
       val expectedConnectedClients = Set(publicKey)
-      val zoneDeliveryId = expectMsgPF() {
+      val zoneDeliveryId = testProbe.expectMsgPF() {
         case ResponseWithIds(Right(JoinZoneResponse(_, `expectedConnectedClients`)), `unusedClientCorrelationId`, _, id) => id
       }
-      lastSender ! MessageReceivedConfirmation(zoneDeliveryId)
+      testProbe.send(
+        testProbe.lastSender,
+        MessageReceivedConfirmation(zoneDeliveryId)
+      )
     }
   }
 

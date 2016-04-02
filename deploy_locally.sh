c2b4110757811650ae06eeb8e2143ed6deec25c4
David Piggott <david@piggott.me.uk>
2016-04-02 14:53:32 +0100

    Fix stage and deploy_locally scripts (use SBT, not Activator)

--- deploy_locally.sh
+++ deploy_locally.sh
@@ -6,7 +6,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rm -rf $DIR/stage && mkdir $DIR/stage
 
 cp cassandra.sh $DIR/stage/
-(cd $DIR && ./activator docker:stage)
+(cd $DIR && sbt docker:stage)
 cp -r $DIR/target/docker/stage $DIR/stage/liquidity
 cp liquidity.sh $DIR/stage/
 cp -r $DIR/nginx $DIR/stage/nginx

efb509471d3dc6ece6e7c3c6e106607005c99371
David Piggott <david@piggott.me.uk>
2016-05-02 16:40:38 +0100

    Remove now-invalid Docker volume line

--- nginx.sh
+++ nginx.sh
@@ -6,7 +6,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 docker run -d -p 443:443 \
     --restart always \
     --name nginx \
-    -v $DIR/nginx/dh2048.pem:/etc/nginx/certs/dh2048.pem:ro \
     -v $DIR/nginx/liquidity.dhpcs.com.crt:/etc/nginx/certs/liquidity.dhpcs.com.crt:ro \
     -v $DIR/nginx/liquidity.dhpcs.com.key:/etc/nginx/certs/liquidity.dhpcs.com.key:ro \
     -v $DIR/nginx/liquidity.dhpcs.com.dhparam.pem:/etc/nginx/certs/liquidity.dhpcs.com.dhparam.pem:ro \

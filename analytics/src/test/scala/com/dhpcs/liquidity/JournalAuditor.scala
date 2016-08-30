5df9a7d19cff133dd3e3c5d2aa985b04bce09d7b
David Piggott <david@piggott.me.uk>
2016-08-30 16:17:09 +0100

    Increase JournalAuditor parallelism
    Also fix minor zone summary formatting issues (newlines).

--- analytics/src/test/scala/com/dhpcs/liquidity/JournalAuditor.scala
+++ analytics/src/test/scala/com/dhpcs/liquidity/JournalAuditor.scala
@@ -6,7 +6,7 @@ import java.util.{Currency, UUID}
 import actors.ZoneValidator._
 import akka.actor.ActorSystem
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
-import akka.persistence.query.{EventEnvelope, PersistenceQuery}
+import akka.persistence.query.PersistenceQuery
 import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
 import com.dhpcs.liquidity.JournalAuditor._
@@ -40,7 +40,7 @@ class JournalAuditor extends AsyncWordSpec with Matchers with BeforeAndAfterAll 
 
   "The journal" must {
     "replay all zones' events" in {
-      def idsAndEvents(): Future[Map[ZoneId, Seq[EventEnvelope]]] = {
+      def readZonesAndBalances(): Future[Seq[(Zone, Map[AccountId, BigDecimal])]] = {
         val readJournal = PersistenceQuery(system)
           .readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
         readJournal
@@ -48,41 +48,40 @@ class JournalAuditor extends AsyncWordSpec with Matchers with BeforeAndAfterAll 
           .collect { case ZoneIdStringPattern(uuidString) =>
             ZoneId(UUID.fromString(uuidString))
           }
-          .mapAsync(1)(zoneId =>
+          .mapAsyncUnordered(sys.runtime.availableProcessors)(zoneId =>
             readJournal
               .currentEventsByPersistenceId(zoneId.toString, 0, Long.MaxValue)
-              .runFold(Seq.empty[EventEnvelope])(_ :+ _)
-              .map(zoneId -> _)
+              .runFold[(Zone, Map[AccountId, BigDecimal])]((null, Map.empty)) { case ((zone, balances), envelope) =>
+              envelope.event match {
+                case zoneCreatedEvent: ZoneCreatedEvent =>
+                  (zoneCreatedEvent.zone, balances)
+                case _: ZoneJoinedEvent =>
+                  (zone, balances)
+                case _: ZoneQuitEvent =>
+                  (zone, balances)
+                case ZoneNameChangedEvent(_, name) =>
+                  (zone.copy(name = name), balances)
+                case MemberCreatedEvent(_, member) =>
+                  (zone.copy(members = zone.members + (member.id -> member)), balances)
+                case MemberUpdatedEvent(_, member) =>
+                  (zone.copy(members = zone.members + (member.id -> member)), balances)
+                case AccountCreatedEvent(_, account) =>
+                  (zone.copy(accounts = zone.accounts + (account.id -> account)), balances)
+                case AccountUpdatedEvent(_, account) =>
+                  (zone.copy(accounts = zone.accounts + (account.id -> account)), balances)
+                case TransactionAddedEvent(_, transaction) =>
+                  val updatedSourceBalance =
+                    balances.getOrElse(transaction.from, BigDecimal(0)) - transaction.value
+                  val updatedDestinationBalance =
+                    balances.getOrElse(transaction.to, BigDecimal(0)) + transaction.value
+                  (zone.copy(transactions = zone.transactions + (transaction.id -> transaction)), balances +
+                    (transaction.from -> updatedSourceBalance) +
+                    (transaction.to -> updatedDestinationBalance))
+              }
+            }
           )
-          .runFold(Map.empty[ZoneId, Seq[EventEnvelope]])(_ + _)
+          .runFold(Seq.empty[(Zone, Map[AccountId, BigDecimal])])(_ :+ _)
       }
-      def zoneAndBalances(events: Seq[Any]): (Zone, Map[AccountId, BigDecimal]) =
-        events.tail.foldLeft(
-          events.head match {
-            case zoneCreatedEvent: ZoneCreatedEvent =>
-              (zoneCreatedEvent.zone, Map.empty[AccountId, BigDecimal].withDefaultValue(BigDecimal(0)))
-          }) { case ((zone, balances), event) =>
-          event match {
-            case _: ZoneJoinedEvent => (zone, balances)
-            case _: ZoneQuitEvent => (zone, balances)
-            case ZoneNameChangedEvent(_, name) =>
-              (zone.copy(name = name), balances)
-            case MemberCreatedEvent(_, member) =>
-              (zone.copy(members = zone.members + (member.id -> member)), balances)
-            case MemberUpdatedEvent(_, member) =>
-              (zone.copy(members = zone.members + (member.id -> member)), balances)
-            case AccountCreatedEvent(_, account) =>
-              (zone.copy(accounts = zone.accounts + (account.id -> account)), balances)
-            case AccountUpdatedEvent(_, account) =>
-              (zone.copy(accounts = zone.accounts + (account.id -> account)), balances)
-            case TransactionAddedEvent(_, transaction) =>
-              val updatedSourceBalance = balances(transaction.from) - transaction.value
-              val updatedDestinationBalance = balances(transaction.to) + transaction.value
-              (zone.copy(transactions = zone.transactions + (transaction.id -> transaction)), balances +
-                (transaction.from -> updatedSourceBalance) +
-                (transaction.to -> updatedDestinationBalance))
-          }
-        }
       def summarise(zoneAndBalances: (Zone, Map[AccountId, BigDecimal])): String = {
         val (zone, balances) = zoneAndBalances
         val unnamed = "<unnamed>"
@@ -97,21 +96,22 @@ class JournalAuditor extends AsyncWordSpec with Matchers with BeforeAndAfterAll 
            |Name: ${zone.name.getOrElse(unnamed)}
            |Created: ${dateFormat.format(zone.created)}
            |Members:\n${
-          zone.members.map { case (memberId, member) =>
+          zone.members.values.toSeq.sortBy(_.name).par.map { member =>
             def balance(memberId: MemberId): BigDecimal =
               zone.accounts.collectFirst {
-                case (accountId, account) if account.ownerMemberIds.contains(memberId) => balances(accountId)
+                case (accountId, account) if account.ownerMemberIds.contains(memberId) =>
+                  balances.getOrElse(accountId, BigDecimal(0))
               }.getOrElse(BigDecimal(0))
             val name = member.name.getOrElse(unnamed)
             val padding = " " * (longestMemberNameLength - name.length)
-            s"    $name: $padding${currencyFormat.format(balance(memberId))}"
+            s"    $name: $padding${currencyFormat.format(balance(member.id))}"
           }.mkString("\n")
         }
            |Transactions:\n${
           val longestTransactionValueLength =
             (zone.transactions.values.map(_.value).map(currencyFormat.format).map(_.length).toSeq :+ 0).max
-          zone.transactions.values.toSeq.sortBy(_.created).map { transaction =>
-            def memberName(accountId: AccountId): String = {
+          zone.transactions.values.toSeq.sortBy(_.created).par.map { transaction =>
+            def name(accountId: AccountId): String = {
               val account = zone.accounts(accountId)
               val name = zone.members.collectFirst {
                 case (memberId, member) if account.ownerMemberIds.contains(memberId) => member.name
@@ -119,32 +119,22 @@ class JournalAuditor extends AsyncWordSpec with Matchers with BeforeAndAfterAll 
               }.flatten.getOrElse(unnamed)
               val padding = " " * (longestMemberNameLength - name.length)
               s"$name$padding"
-
             }
-            val from = memberName(transaction.from)
-            val to = memberName(transaction.to)
+            val from = name(transaction.from)
+            val to = name(transaction.to)
             val value = currencyFormat.format(transaction.value)
             val padding = " " * (longestTransactionValueLength - value.length)
             val created = transaction.created
             s"    $from sent $value$padding to $to on ${dateFormat.format(created)}"
           }.mkString("\n")
-        }
-          """.stripMargin
+        }""".stripMargin
       }
       for {
-        zonesAndBalances <- idsAndEvents().map(
-          _.values
-            .map(
-              _.map(_.event)
-            )
-            .map(zoneAndBalances)
-            .toSeq
-            .sortBy { case (zone, balances) => zone.created }
-        )
+        zonesAndBalances <- readZonesAndBalances()
+        sortedZonesAndBalances = zonesAndBalances.sortBy { case (zone, _) => zone.created }
+        sortedSummaries = sortedZonesAndBalances.map(summarise)
       } yield {
-        println(
-          zonesAndBalances.map(summarise).mkString("\n")
-        )
+        println(sortedSummaries.mkString("", "\n", "\n"))
         println(s"Restored ${zonesAndBalances.size} zones")
         succeed
       }

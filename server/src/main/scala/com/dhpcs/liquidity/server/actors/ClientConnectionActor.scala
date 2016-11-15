dhpiggott/liquidity-service

commit 73a7c2ec2fd75f30e63bb90a63f20b2dffe5ee29

Remove redundant message sends that were causing dead-letters log spam

server/src/main/scala/com/dhpcs/liquidity/server/actors/ClientConnectionActor.scala
@@ -102,10 +102,8 @@ object ClientConnectionActor {
             override def receive = {
               case Status.Success(_) | Status.Failure(_) =>
                 flowActor ! PoisonPill
-                outActor ! Status.Success(())
               case _: Terminated =>
                 context.stop(self)
-                outActor ! Status.Success(())
               case other =>
                 flowActor ! other
             }

commit dbaa0ebfdd9dbb54eb524879456cfe31c25522a2

Fix regression introduced by 73a7c2ec2fd75f30e63bb90a63f20b2dffe5ee29
The regression was that client initiated disconnections weren't completing,
which meant that subsequent reconnects were never even attempted, as they
blocked waiting on the pending disconnect. The regression also manifested as
warnings in the logs when running the LiquidityServerSpec integration test.

server/src/main/scala/com/dhpcs/liquidity/server/actors/ClientConnectionActor.scala
@@ -105,10 +105,11 @@ object ClientConnectionActor {
             val flowActor = context.watch(context.actorOf(props(outActor), name))
             override def supervisorStrategy: SupervisorStrategy = SupervisorStrategy.stoppingStrategy
             override def receive = {
-              case Status.Success(_) | Status.Failure(_) =>
+              case _: Status.Success | _: Status.Failure =>
                 flowActor ! PoisonPill
               case _: Terminated =>
                 context.stop(self)
+                outActor ! Status.Success(())
               case other =>
                 flowActor.forward(other)
             }
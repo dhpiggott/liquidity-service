c449c70abce7ecb119bd5466b80a63bd7291bbec
David Piggott <david@piggott.me.uk>
2017-05-21 11:20:49 +0100

    Resolve ProtoConverter derivation TODOs
    The resolution was to configure IntelliJ to always consider imports of
    model._ to be valid. Not idea, but better than leaving this as it was
    indefinitely.

--- server/src/main/scala/com/dhpcs/liquidity/server/serialization/ZoneEventSerializer.scala
+++ server/src/main/scala/com/dhpcs/liquidity/server/serialization/ZoneEventSerializer.scala
@@ -1,7 +1,6 @@
 package com.dhpcs.liquidity.server.serialization
 
 import akka.actor.ExtendedActorSystem
-// TODO: Why doesn't IntelliJ recognise that this is needed?
 import com.dhpcs.liquidity.model._
 import com.dhpcs.liquidity.persistence._
 import com.dhpcs.liquidity.proto

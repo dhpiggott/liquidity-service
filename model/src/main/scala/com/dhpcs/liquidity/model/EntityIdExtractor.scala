c88f0aeb1c26b81b500c9704cf2623be0d64f40d
David Piggott <david@piggott.me.uk>
2017-06-10 08:46:20 +0100

    Re-enable cross building for Scala 2.11.11 and 2.12.2

--- model/src/main/scala/com/dhpcs/liquidity/model/EntityIdExtractor.scala
+++ model/src/main/scala/com/dhpcs/liquidity/model/EntityIdExtractor.scala
@@ -5,5 +5,7 @@ trait EntityIdExtractor[E, I] {
 }
 
 object EntityIdExtractor {
-  def instance[E, I](apply: E => I): EntityIdExtractor[E, I] = (e: E) => apply(e)
+  def instance[E, I](apply: E => I): EntityIdExtractor[E, I] = new EntityIdExtractor[E, I] {
+    override def extractId(e: E): I = apply(e)
+  }
 }
--- project/CommonProjectSettingsPlugin.scala
+++ project/CommonProjectSettingsPlugin.scala
@@ -6,50 +6,56 @@ object CommonProjectSettingsPlugin extends AutoPlugin {
 
   private lazy val scalaSettings = Seq(
     scalaVersion := "2.12.2",
-    // See https://tpolecat.github.io/2017/04/25/scalac-flags.html for explanations.
-    scalacOptions in Compile ++= Seq(
-      "-deprecation",
-      "-encoding",
-      "utf-8",
-      "-explaintypes",
-      "-feature",
-      "-unchecked",
-      // TODO: Reorganise the Protobuf containing modules such that _just_ the generated code can be excluded from
-      // all these checks, then enable fatal-warnings.
-      // "-Xfatal-warnings",
-      "-Xlint:adapted-args",
-      "-Xlint:by-name-right-associative",
-      "-Xlint:constant",
-      "-Xlint:delayedinit-select",
-      "-Xlint:doc-detached",
-      "-Xlint:inaccessible",
-      "-Xlint:infer-any",
-      "-Xlint:missing-interpolator",
-      "-Xlint:nullary-override",
-      "-Xlint:nullary-unit",
-      "-Xlint:option-implicit",
-      "-Xlint:package-object-classes",
-      "-Xlint:poly-implicit-overload",
-      "-Xlint:private-shadow",
-      "-Xlint:stars-align",
-      "-Xlint:type-parameter-shadow",
-      "-Xlint:unsound-match",
-      "-Yno-adapted-args",
-      "-Ywarn-dead-code",
-      "-Ywarn-extra-implicit",
-      "-Ywarn-inaccessible",
-      "-Ywarn-infer-any",
-      "-Ywarn-nullary-override",
-      "-Ywarn-nullary-unit",
-      "-Ywarn-numeric-widen",
-      "-Ywarn-unused:implicits",
-      "-Ywarn-unused:imports",
-      "-Ywarn-unused:locals",
-      "-Ywarn-unused:params",
-      "-Ywarn-unused:patvars",
-      "-Ywarn-unused:privates",
-      "-Ywarn-value-discard"
-    )
+    crossScalaVersions := Seq("2.11.11", "2.12.2"),
+    // See https://tpolecat.github.io/2017/04/25/scalac-flags.html for explanations. 2.11 doesn't support all of these,
+    // so we simply don't set any of them when building for 2.11. The 2.12 build will pick up any issues anyway.
+    scalacOptions in Compile ++= (CrossVersion.binaryScalaVersion(scalaVersion.value) match {
+      case "2.12" =>
+        Seq(
+          "-deprecation",
+          "-encoding",
+          "utf-8",
+          "-explaintypes",
+          "-feature",
+          "-unchecked",
+          // TODO: Reorganise the Protobuf containing modules such that _just_ the generated code can be excluded from
+          // all these checks, then enable fatal-warnings.
+          // "-Xfatal-warnings",
+          "-Xlint:adapted-args",
+          "-Xlint:by-name-right-associative",
+          "-Xlint:constant",
+          "-Xlint:delayedinit-select",
+          "-Xlint:doc-detached",
+          "-Xlint:inaccessible",
+          "-Xlint:infer-any",
+          "-Xlint:missing-interpolator",
+          "-Xlint:nullary-override",
+          "-Xlint:nullary-unit",
+          "-Xlint:option-implicit",
+          "-Xlint:package-object-classes",
+          "-Xlint:poly-implicit-overload",
+          "-Xlint:private-shadow",
+          "-Xlint:stars-align",
+          "-Xlint:type-parameter-shadow",
+          "-Xlint:unsound-match",
+          "-Yno-adapted-args",
+          "-Ywarn-dead-code",
+          "-Ywarn-extra-implicit",
+          "-Ywarn-inaccessible",
+          "-Ywarn-infer-any",
+          "-Ywarn-nullary-override",
+          "-Ywarn-nullary-unit",
+          "-Ywarn-numeric-widen",
+          "-Ywarn-unused:implicits",
+          "-Ywarn-unused:imports",
+          "-Ywarn-unused:locals",
+          "-Ywarn-unused:params",
+          "-Ywarn-unused:patvars",
+          "-Ywarn-unused:privates",
+          "-Ywarn-value-discard"
+        )
+      case "2.11" => Seq("-encoding", "utf-8")
+    })
   )
 
   private lazy val resolverSettings = Seq(

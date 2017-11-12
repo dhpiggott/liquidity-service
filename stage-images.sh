3ad1492c75fe2cade8d5df572e12d09a5ecd071c
David Piggott <david@piggott.me.uk>
2017-11-12 12:25:30 +0000

    Don't stage pre-RDS helper scripts

--- stage-images.sh
+++ stage-images.sh
@@ -25,12 +25,6 @@ mkdir $DIR/image-stage
 cp --recursive $DIR/server/target/docker/stage $DIR/image-stage/server
 cp $DIR/docker-compose.yml $DIR/image-stage/
 cp $DIR/docker-compose.$TYPE.yml $DIR/image-stage/docker-compose.override.yml
-cp $DIR/journal.sql $DIR/image-stage/
-cp $DIR/init-journal.sh $DIR/image-stage/
-cp $DIR/analytics.sql $DIR/image-stage/
-cp $DIR/init-analytics.sh $DIR/image-stage/
-cp $DIR/save-journal.sh $DIR/image-stage/
-cp $DIR/load-journal.sh $DIR/image-stage/
 
 rsync --archive \
     --human-readable \

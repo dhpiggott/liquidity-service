ff1e6cf4cb9e63fb22b72102cd73e61b172ff02d
David Piggott <david@piggott.me.uk>
2017-11-24 20:06:09 +0000

    Publish to ECR as part of CI

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-eval $(aws ecr get-login --no-include-email --region eu-west-2)
-
-(cd $DIR && sbt validate server/docker:publish)

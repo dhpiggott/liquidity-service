9dfbb701fba4e613b2efe4181494284f70a21bea
David Piggott <david@piggott.me.uk>
2018-03-09 20:45:42 +0000

    Convert EcsSimpleServiceDiscovery to use non-blocking AWS SDK

--- server/src/main/scala/com/dhpcs/liquidity/server/EcsSimpleServiceDiscovery.scala
+++ server/src/main/scala/com/dhpcs/liquidity/server/EcsSimpleServiceDiscovery.scala
@@ -6,14 +6,14 @@ import akka.actor.ActorSystem
 import akka.discovery.SimpleServiceDiscovery
 import akka.discovery.SimpleServiceDiscovery.{Resolved, ResolvedTarget}
 import akka.pattern.after
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.ecs.model._
-import com.amazonaws.services.ecs.{AmazonECS, AmazonECSClientBuilder}
 import com.dhpcs.liquidity.server.EcsSimpleServiceDiscovery._
+import software.amazon.awssdk.core.regions.Region
+import software.amazon.awssdk.services.ecs._
+import software.amazon.awssdk.services.ecs.model._
 
-import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 import scala.collection.immutable.Seq
+import scala.compat.java8.FutureConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
@@ -22,30 +22,30 @@ class EcsSimpleServiceDiscovery(system: ActorSystem)
 
   override def lookup(name: String,
                       resolveTimeout: FiniteDuration): Future[Resolved] = {
-//    val ecsClient = AmazonECSClientBuilder.defaultClient()
+//    val ecsClient = ECSAsyncClient.create()
     val ecsClient =
-      AmazonECSClientBuilder.standard().withRegion(Regions.US_EAST_1).build()
-    import system.dispatcher
+      ECSAsyncClient.builder().region(Region.US_EAST_1).build()
+    implicit val ec: ExecutionContext = system.dispatcher
     Future.firstCompletedOf(
       Seq(
         after(resolveTimeout, using = system.scheduler)(
           Future.failed(new TimeoutException("Future timed out!"))
         ),
-        // TODO: Async client?
-        Future {
-          Resolved(
-            serviceName = name,
-            addresses = for {
-              task <- resolveTasks(ecsClient, name)
-              container <- task.getContainers.asScala
-              networkInterface <- container.getNetworkInterfaces.asScala
-            } yield
-              ResolvedTarget(
-                host = networkInterface.getPrivateIpv4Address,
-                port = None
-              )
+        resolveTasks(ecsClient, name).map(
+          tasks =>
+            Resolved(
+              serviceName = name,
+              addresses = for {
+                task <- tasks
+                container <- task.containers().asScala
+                networkInterface <- container.networkInterfaces().asScala
+              } yield
+                ResolvedTarget(
+                  host = networkInterface.privateIpv4Address(),
+                  port = None
+                )
           )
-        }
+        )
       )
     )
   }
@@ -68,46 +68,62 @@ object EcsSimpleServiceDiscovery {
       atMost = 10.seconds
     )
 
-  private def resolveTasks(ecsClient: AmazonECS,
-                           serviceName: String): Seq[Task] =
-    describeTasks(ecsClient, listTaskArns(ecsClient, serviceName))
+  private def resolveTasks(ecsClient: ECSAsyncClient, serviceName: String)(
+      implicit ec: ExecutionContext): Future[Seq[Task]] =
+    for {
+      taskArns <- listTaskArns(ecsClient, serviceName)
+      task <- describeTasks(ecsClient, taskArns)
+    } yield task
 
-  @tailrec private[this] def listTaskArns(
-      ecsClient: AmazonECS,
-      serviceName: String,
-      pageTaken: Option[String] = None,
-      accumulator: Seq[String] = Seq.empty): Seq[String] = {
-    val listTasksResult = ecsClient.listTasks(
-      new ListTasksRequest()
-        .withServiceName(serviceName)
-        .withNextToken(pageTaken.orNull)
-        .withDesiredStatus(DesiredStatus.RUNNING)
-    )
-    val accumulatedTasksArns = accumulator ++ listTasksResult.getTaskArns.asScala
-    listTasksResult.getNextToken match {
-      case null =>
-        accumulatedTasksArns
-
-      case nextPageToken =>
-        listTaskArns(
-          ecsClient,
-          serviceName,
-          Some(nextPageToken),
-          accumulatedTasksArns
+  private[this] def listTaskArns(ecsClient: ECSAsyncClient,
+                                 serviceName: String,
+                                 pageTaken: Option[String] = None,
+                                 accumulator: Seq[String] = Seq.empty)(
+      implicit ec: ExecutionContext): Future[Seq[String]] =
+    for {
+      listTasksResponse <- toScala(
+        ecsClient.listTasks(
+          ListTasksRequest
+            .builder()
+            .serviceName(serviceName)
+            .nextToken(pageTaken.orNull)
+            .desiredStatus(DesiredStatus.RUNNING)
+            .build()
         )
-    }
-  }
+      )
+      accumulatedTasksArns = accumulator ++ listTasksResponse.taskArns().asScala
+      taskArns <- listTasksResponse.nextToken() match {
+        case null =>
+          Future.successful(accumulatedTasksArns)
 
-  private[this] def describeTasks(ecsClient: AmazonECS,
-                                  taskArns: Seq[String]): Seq[Task] =
+        case nextPageToken =>
+          listTaskArns(
+            ecsClient,
+            serviceName,
+            Some(nextPageToken),
+            accumulatedTasksArns
+          )
+      }
+    } yield taskArns
+
+  private[this] def describeTasks(
+      ecsClient: ECSAsyncClient,
+      taskArns: Seq[String])(implicit ec: ExecutionContext): Future[Seq[Task]] =
     for {
       // Each DescribeTasksRequest can contain at most 100 task ARNs.
-      group <- taskArns.grouped(100).to[Seq]
-      tasks = ecsClient.describeTasks(
-        new DescribeTasksRequest()
-          .withTasks(group.asJava)
-      )
-      task <- tasks.getTasks.asScala
-    } yield task
+      describeTasksResponses <- Future
+        .traverse(taskArns.grouped(100))(
+          taskArnGroup =>
+            toScala(
+              ecsClient.describeTasks(
+                DescribeTasksRequest
+                  .builder()
+                  .tasks(taskArnGroup.asJava)
+                  .build()
+              )
+          )
+        )
+      tasks = describeTasksResponses.flatMap(_.tasks().asScala).to[Seq]
+    } yield tasks
 
 }
